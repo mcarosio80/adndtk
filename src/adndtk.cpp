fae06c8ae9d6db8d2ce08ad7907d76ff9fe7baed
Marco Carosio <marco.carosio@gmail.com>
2023-03-10 21:18:39 +0100

    Add execution of generic prepared statements

--- src/adndtk.cpp
+++ src/adndtk.cpp
@@ -1,6 +1,7 @@
 #include "adndtk.h"
 
 #include <sstream>
+#include <cstring>
 
 bool Adndtk::Cyclopedia::_initialised = false;
 
@@ -11,6 +12,7 @@ Adndtk::Cyclopedia& Adndtk::Cyclopedia::get_instance()
     if (!_initialised)
     {
         _instance.init();
+        _initialised = true;
     }
     return _instance;
 }
@@ -19,7 +21,13 @@ bool Adndtk::Cyclopedia::init()
 {
     int ret = sqlite3_open_v2(Settings::database_path, &_dbConn, SQLITE_OPEN_READWRITE | SQLITE_OPEN_URI, NULL);
     
-    return check_state(ret);
+    bool ok = check_state(ret);
+    if (ok)
+    {
+        //select json_group_array(json_array(ID, ACRONYM, DESCRIPTION)) from coin
+        prepare_statement("select id, acronym, description from coin where id = ?", Query::select_coin), QueryType::plain;
+    }
+    return ok;
 }
 
 Adndtk::Cyclopedia::Cyclopedia()
@@ -35,7 +43,7 @@ Adndtk::Cyclopedia::~Cyclopedia()
 
 bool Adndtk::Cyclopedia::check_state(int return_code)
 {
-    if (errno != SQLITE_OK)
+    if (return_code != SQLITE_OK)
 	{
         std::stringstream ss;
         ss << sqlite3_errstr(return_code) << ": " << sqlite3_errmsg(_dbConn);
@@ -44,4 +52,28 @@ bool Adndtk::Cyclopedia::check_state(int return_code)
         return false;
     }
     return true;
+}
+
+void Adndtk::Cyclopedia::prepare_statement(const char *stmt, const Query& queryId, const QueryType& queryType/* = Settings::query_type*/)
+{
+    sqlite3_stmt *pStmt = nullptr;
+    std::string selectStmt{stmt, std::strlen(stmt)};
+
+    if (queryType == QueryType::json)
+    {
+        std::string token = "select ";
+        auto it = selectStmt.find(token);
+        selectStmt.replace(it, token.length(), "select json_group_array(json_array(");
+        
+        token = " from";
+        it = selectStmt.find(token);
+        selectStmt.replace(it, token.length(), ")) from");
+    }
+
+    int ret = sqlite3_prepare_v2(_dbConn, selectStmt.c_str(), static_cast<int>(selectStmt.length()), &pStmt, NULL);
+
+    if (check_state(ret))
+    {
+        _statements[queryId] = pStmt;
+    }
 }
--- src/adndtk.h
+++ src/adndtk.h
@@ -3,15 +3,51 @@
 #include "../generated/defs.h"
 #include "../generated/config.h"
 #include <sqlite3.h>
+#include <map>
+#include <functional>
 
 namespace Adndtk
 {
     class Cyclopedia
     {
     public:
+
+        enum class Query
+        {
+            select_coin
+        };
+
+        using OnQueryResult = std::function<void (int, int, const char *, const unsigned char *)>;
+
         static Cyclopedia& get_instance();
         Cyclopedia(Cyclopedia const&) = delete;
         void operator=(Cyclopedia const&) = delete;
+
+		template<class... _T>
+        void exec_prepared_statement(const Query& queryId, const OnQueryResult& cbkOnResult, _T... values)
+		{
+            if (_statements.find(queryId) == _statements.end())
+            {
+                ErrorManager::get_instance().error("Unable to find prepared statement for query ID");
+                return;
+            }
+            
+            auto params = std::make_tuple<_T...>(std::move(values...));
+            ParamsBinder<decltype(params), sizeof...(_T)>::bind_values(*this, queryId, params);
+
+            int rowCount = 0;
+            auto stmt = _statements[queryId];
+            int columnCount = sqlite3_column_count(stmt);
+            while (sqlite3_step(stmt) == SQLITE_ROW)
+            {
+                for (int col = 0; col<columnCount; ++col)
+                {
+                    auto colType = sqlite3_column_decltype(stmt, col);
+                    auto colValue = sqlite3_column_text(stmt, col);
+                    cbkOnResult(rowCount, col, colType, colValue);
+                }
+            }
+        }
 
     private:
         Cyclopedia();
@@ -19,8 +55,41 @@ namespace Adndtk
 
         bool init();
         bool check_state(int return_code);
+        void prepare_statement(const char *stmt, const Query& queryId, const QueryType& queryType = Settings::query_type);
+
+        void bind(const Query& queryId, int position, int value)
+        {
+            auto& stmt = _statements[queryId];
+            sqlite3_bind_int(stmt, position, value);
+        }
+
+        void bind(const Query& queryId, int position, double value)
+        {
+            auto& stmt = _statements[queryId];
+            sqlite3_bind_double(stmt, position, value);
+        }
+
+        template<class ParamTuple, std::size_t PackSize>
+		struct ParamsBinder
+		{
+			static void bind_values(Cyclopedia& cp, const Query& queryId, const ParamTuple& t)
+			{
+				ParamsBinder<ParamTuple, PackSize - 1>::bind_values(queryId, t);
+                cp.bind(queryId, PackSize, std::get<PackSize - 1>(t));
+			}
+		};
+
+		template<class ParamTuple>
+		struct ParamsBinder<ParamTuple, 1>
+		{
+			static void bind_values(Cyclopedia& cp, const Query& queryId, const ParamTuple& t)
+            {
+                cp.bind(queryId, 1, std::get<0>(t));
+            }
+		};
 
 		static bool     _initialised;
         sqlite3         *_dbConn;
+        std::map<Query, sqlite3_stmt*>      _statements;
     };
 }
