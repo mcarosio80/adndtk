a212f2b857194f633ec06b09686aff26b766b2cf
Marco Carosio <marco.carosio@gmail.com>
2023-04-06 21:17:07 +0200

    Add tests for demihumans limits to experience

--- test/test.cpp
+++ test/test.cpp
@@ -2587,13 +2587,109 @@ TEST_CASE("[TC-EXPE.008] Experience gain/loss changes level accordingly for bard
 
 TEST_CASE("[TC-EXPE.009] Experience gain for demihumans (halfling) cannot exceed the limits", "[experience]" )
 {
-    Defs::character_class cls = Defs::character_class::cleric;
+    std::map<Defs::character_class, ExperienceLevel> limits
+    {
+        {Defs::character_class::fighter, 9},
+        {Defs::character_class::cleric, 8},
+        {Defs::character_class::thief, 15}
+    };
 
-    Experience exp{cls, Defs::race::halfling};
+    XP xp = 2000000;
+    for (auto& l : limits)
+    {
+        Experience exp{l.first, Defs::race::halfling};
+        exp += xp;
+        REQUIRE(exp.xp() == xp);
+        REQUIRE(exp.level() == l.second);
+    }
+}
 
-    exp += 500000;
-    REQUIRE(exp.xp() == 500000);
-    REQUIRE(exp.level() == 8);
+TEST_CASE("[TC-EXPE.010] Experience gain for demihumans (elf) cannot exceed the limits", "[experience]" )
+{
+    std::map<Defs::character_class, ExperienceLevel> limits
+    {
+        {Defs::character_class::fighter, 12},
+        {Defs::character_class::ranger, 15},
+        {Defs::character_class::mage, 15},
+        {Defs::character_class::diviner, 15},
+        {Defs::character_class::enchanter, 15},
+        {Defs::character_class::cleric, 12},
+        {Defs::character_class::thief, 12}
+    };
+
+    XP xp = 3000000;
+    for (auto& l : limits)
+    {
+        Experience exp{l.first, Defs::race::elf};
+        exp += xp;
+        REQUIRE(exp.xp() == xp);
+        REQUIRE(exp.level() == l.second);
+    }
+}
+
+TEST_CASE("[TC-EXPE.010] Experience gain for demihumans (half_elf) cannot exceed the limits", "[experience]" )
+{
+    std::map<Defs::character_class, ExperienceLevel> limits
+    {
+        {Defs::character_class::fighter, 14},
+        {Defs::character_class::ranger, 16},
+        {Defs::character_class::mage, 12},
+        {Defs::character_class::conjurer, 12},
+        {Defs::character_class::diviner, 12},
+        {Defs::character_class::enchanter, 12},
+        {Defs::character_class::transmuter, 12},
+        {Defs::character_class::cleric, 14},
+        {Defs::character_class::druid, 9},
+        {Defs::character_class::thief, 12}
+    };
+
+    XP xp = 3000000;
+    for (auto& l : limits)
+    {
+        Experience exp{l.first, Defs::race::half_elf};
+        exp += xp;
+        REQUIRE(exp.xp() == xp);
+        REQUIRE(exp.level() == l.second);
+    }
+}
+
+TEST_CASE("[TC-EXPE.011] Experience gain for demihumans (dwarf) cannot exceed the limits", "[experience]" )
+{
+    std::map<Defs::character_class, ExperienceLevel> limits
+    {
+        {Defs::character_class::fighter, 15},
+        {Defs::character_class::cleric, 10},
+        {Defs::character_class::thief, 12}
+    };
+
+    XP xp = 3000000;
+    for (auto& l : limits)
+    {
+        Experience exp{l.first, Defs::race::dwarf};
+        exp += xp;
+        REQUIRE(exp.xp() == xp);
+        REQUIRE(exp.level() == l.second);
+    }
+}
+
+TEST_CASE("[TC-EXPE.012] Experience gain for demihumans (gnome) cannot exceed the limits", "[experience]" )
+{
+    std::map<Defs::character_class, ExperienceLevel> limits
+    {
+        {Defs::character_class::fighter, 11},
+        {Defs::character_class::illusionist, 15},
+        {Defs::character_class::cleric, 9},
+        {Defs::character_class::thief, 13}
+    };
+
+    XP xp = 3000000;
+    for (auto& l : limits)
+    {
+        Experience exp{l.first, Defs::race::gnome};
+        exp += xp;
+        REQUIRE(exp.xp() == xp);
+        REQUIRE(exp.level() == l.second);
+    }
 }
 
 TEST_CASE("[TC-HITP.001] Increasing length of HP sequences generates new HD values", "[HP]" )
