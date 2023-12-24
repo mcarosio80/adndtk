537b24920e6c44b0bc35ff0ca54c9aa4030e0ef3
Marco Carosio <marco.carosio@gmail.com>
2023-12-24 14:46:11 +0100

    Set c++20 standard

--- vendor/rapidjson.cmake
+++ vendor/rapidjson.cmake
@@ -3,7 +3,7 @@ ExternalProject_Add(
     rapidjson
     PREFIX "vendor/rapidjson"
     GIT_REPOSITORY "https://github.com/Tencent/rapidjson.git"
-    GIT_TAG f54b0e47a08782a6131cc3d60f94d038fa6e0a51
+    GIT_TAG 6089180ecb704cb2b136777798fa1be303618975
     TIMEOUT 10
     CMAKE_ARGS
         -DRAPIDJSON_BUILD_TESTS=OFF
