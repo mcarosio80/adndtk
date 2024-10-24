b9c23ca74aa269def700283a7cf79a5fcf71a031
Marco Carosio <marco.carosio@gmail.com>
2024-10-24 21:33:12 +0200

    Add retrieve of max age for characters

--- src/character.h
+++ src/character.h
@@ -92,6 +92,7 @@ namespace Adndtk
 
         // Racial stats
         inline const short& age() const { return _racialStats.current_age(); };
+        inline const short& max_age() const { return _racialStats.max_age(); };
         inline bool grow_old(const short& years) { return _racialStats.grow_old(years); };
         inline bool rejuvenate(const short& years) { return _racialStats.rejuvenate(years); };
         inline Defs::aging_effects get_age_range() const { return _racialStats.get_age_range(); };
